776b4997ae3a80518b9278bc18db708d278be28c
Polaroid339 <victor339335@gmail.com>
2025-04-12 10:43:38 -0300

    commit

---
+++ src/main/java/com/escola/senai/model/Aluno.java
@@ -0,0 +1,49 @@
+package com.escola.senai.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String nomeAluno;
+    private String matriculaAluno;
+    private int idadeAluno;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        this.nomeAluno = nomeAluno;
+    }
+
+    public String getMatriculaAluno() {
+        return matriculaAluno;
+    }
+
+    public void setMatriculaAluno(String matriculaAluno) {
+        this.matriculaAluno = matriculaAluno;
+    }
+
+    public int getIdadeAluno() {
+        return idadeAluno;
+    }
+
+    public void setIdadeAluno(int idadeAluno) {
+        this.idadeAluno = idadeAluno;
+    }
+}
